c020409ab73b440541aa1bf2de71646cdbf9fbdd
Luis Irio <luis.irio@nos.pt>
2023-05-16 10:39:23 +0100

    Mock to_csv

--- life_expectancy/tests/test_main.py
+++ life_expectancy/tests/test_main.py
@@ -1,5 +1,7 @@
 """Tests for the main module"""
 
+from unittest import mock
+
 import pandas as pd
 
 from life_expectancy.main import main
@@ -7,8 +9,11 @@ from life_expectancy.main import main
 
 def test_main(pt_life_expectancy_expected):
     """Run the `main` function and compare the output to the expected output"""
-    pt_life_expectancy_actual = main('PT').reset_index(drop=True)
+    with mock.patch.object(pd.DataFrame, "to_csv") as mock_to_csv:
+        mock_to_csv.side_effect = print("Message: Dataframe saved to CSV")
 
-    pd.testing.assert_frame_equal(
-        pt_life_expectancy_actual, pt_life_expectancy_expected
-    )
+        pt_life_expectancy_actual = main('PT').reset_index(drop=True)
+
+        pd.testing.assert_frame_equal(
+            pt_life_expectancy_actual, pt_life_expectancy_expected
+        )
